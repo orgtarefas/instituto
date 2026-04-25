420beea0a74ccb4a902238f129d2a4587dec9478
orgtarefas <orgtarefas.site@gmail.com>
2026-04-25 10:11:28 -0300

    Add files via upload

---
+++ instituto/scripts_telas/funcionarios_cadastros.js
@@ -0,0 +1,19 @@
+export async function renderizar(container, userData) {
+    container.innerHTML = `
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-users-gear fa-3x text-primary mb-3"></i>
+                        <h2>Gestão de Cadastros</h2>
+                        <p class="lead">Tela em Desenvolvimento</p>
+                        <div class="alert alert-info">
+                            <i class="fas fa-info-circle me-2"></i>
+                            Esta funcionalidade estará disponível em breve!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
